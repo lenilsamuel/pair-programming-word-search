ab4e961b2b3df8520d52ea13c95cef00b8decf5f
lenilsamuel <lenilvarghese@gmail.com>
2021-11-18 19:32:12 -0700

    completed logic for wordsearch function to find letter horizontally and vertically

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,15 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = require("./transpose");
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  const transposeArr = transpose(letters);
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (l of transposeArr) {
+    if (l.join("").includes(word)) return true;
+  }
+  return false;
+};
+
+module.exports = wordSearch;
